4973957d4760177761c4d6e5a451d57d6eb8b4b1
sharova <sv_sharova@mail.ru>
2019-02-15 21:32:11 +0300

    Styling lg

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,8 @@
+import $ from 'jquery';
+import slick from 'slick-carousel';
+
+
+
 // function ready(fn) {
 //   if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
 //     fn();
@@ -24,3 +29,18 @@ function menuVisible(e) {
   toggle.classList.toggle('main-nav__toggle--close');
   menu.classList.toggle('main-nav__list--open');
 };
+
+//Подключение слайдера на главной
+$(function(){
+  $('.slider__block').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    arrows: true,
+    appendArrows: $('.slider__block'),
+    prevArrow:'<button class="slick-prev" aria-label="Previous" type="button"></button>',
+    nextArrow:'<button class="slick-next" aria-label="Next" type="button"></button>',
+    adaptiveHeight: true
+  });
+});
